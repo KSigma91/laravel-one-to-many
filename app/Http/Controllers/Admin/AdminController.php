aed1fead220f1f78a3c0e1f4115260adc449d456
KSigma91 <andrea19912014@gmail.com>
2022-07-25 15:28:48 +0200

    first commit

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class AdminController extends Controller
+{
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function dashboard()
+    {
+        $user = Auth::user();
+        //dump($user->toArray());
+        //dump(Auth::check());
+        return view('admin.dashboard');
+    }
+}
